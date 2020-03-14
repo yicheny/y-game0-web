6e60f2f8f92adffb6d39d83edf80f8a4b884dd79
YLF <1463020554@qq.com>
2020-03-14 23:07:41 +0800

    A 添加首页音频与初始菜单

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Home.scss';
+import {Button} from "../component";
+import homeAudio from '../web-data/audio/home.mp3';
 
 function Home(props) {
-    return <div className="gameBox">
-        <div className="home card">
-
+    return <div className="gameBox flex center">
+        <div className="home card flex center-m">
+            <div className="home-menu flex y">
+                <Button onClick={()=>console.log('开始游戏')}>开始游戏</Button>
+                <Button>退出游戏</Button>
+            </div>
         </div>
+        <audio src={homeAudio} autoPlay loop="loop"/>
     </div>
 }
 
